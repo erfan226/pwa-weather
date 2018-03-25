6f482d20b64c53a766f7e0a5c6e60cf67034adc7
Erfan <Erfan226@gmail.com>
2018-03-26 02:50:24 +0430

    Update app.js

--- app.js
+++ app.js
@@ -37,15 +37,8 @@ app.get('/weather', (req, res)=>{
     }
   });
 })
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-app.listen(server_port, server_ip_address, function () {
-
-    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
-
+app.listen(3000, () => {
+  console.log('Weather app listening on port 3000!');
 });
-// app.listen(3000, () => {
-//   console.log('Weather app listening on port 3000!');
-// });
+
